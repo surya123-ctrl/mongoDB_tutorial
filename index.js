488a13c4426d6e460313311e1286e5077b898c52
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-10-25 02:40:11 +0530

    learned get and put to mongo

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+const express = require('express');
+const server = express();
+const { dbConnection } = require('./db');
+//get
+server.get('/users', async (req, res) => {
+    try {
+        const db = await dbConnection();
+        const collection = db.collection('Users');
+        const users = await collection.find().toArray();
+        res.json(users);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+//post
+server.use(express.json());
+server.post('/users', async (req, res) => {
+    try {
+        const db = await dbConnection()
+        const collection = db.collection('Users')
+        const result = await collection.insertOne(req.body);
+        res.json('Updated');
 
-const collection = db.collection('Users');
-const findResult = await collection.find().toArray();
-console.log(findResult);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+server.listen(3000, () => {
+    console.log("Server started")
+});
